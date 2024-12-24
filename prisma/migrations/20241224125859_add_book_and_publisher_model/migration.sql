83d7b6318c8d02d907491e153dd291b2a4101eae
Fathurrahman <10190031@bsi.ac.id>
2024-12-24 20:03:55 +0700

    fix prisma

--- prisma/migrations/20241224125859_add_book_and_publisher_model/migration.sql
+++ prisma/migrations/20241224125859_add_book_and_publisher_model/migration.sql
@@ -1,31 +1,29 @@
 -- CreateTable
 CREATE TABLE "publishers" (
-    "id" TEXT NOT NULL,
+    "id" VARCHAR(255) NOT NULL,
     "name" VARCHAR(255) NOT NULL,
     "address" VARCHAR(255) NOT NULL,
     "city" VARCHAR(255) NOT NULL,
     "phone" VARCHAR(15) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "publishers_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "books" (
-    "id" TEXT NOT NULL,
+    "id" VARCHAR(255) NOT NULL,
     "category" VARCHAR(255) NOT NULL,
     "title" VARCHAR(255) NOT NULL,
     "price" INTEGER NOT NULL,
     "stock" INTEGER NOT NULL,
     "publisherId" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "publishers_id_key" ON "publishers"("id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "books_id_key" ON "books"("id");
 
 -- AddForeignKey
 ALTER TABLE "books" ADD CONSTRAINT "books_publisherId_fkey" FOREIGN KEY ("publisherId") REFERENCES "publishers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
