d088ecbe5c4d07b84d629c0b0de3dc6f4d624179
Fathurrahman <10190031@bsi.ac.id>
2024-12-25 09:36:48 +0700

    refactor admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,22 +4,13 @@ import DeleteBook from "./delete-book";
 import UpdateBook from "./update-book";
 import TabAdminMenu from "./tab-admin-menu";
 
-const getBooks = async () => {
-  const response = await prisma.book.findMany({
-    include: {
-      publisher: true,
-    },
-  });
-  return response;
-};
-
-const getPublisher = async () => {
-  const response = await prisma.publisher.findMany();
-  return response;
-};
-
 const Product = async () => {
-  const [books, publishers] = await Promise.all([getBooks(), getPublisher()]);
+  const [books, publishers] = await Promise.all([
+    prisma.book.findMany({
+      include: { publisher: true },
+    }),
+    prisma.publisher.findMany(),
+  ]);
   return (
     <div>
       <TabAdminMenu books={books} publishers={publishers} />
