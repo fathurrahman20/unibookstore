fe49872380e314b8368078e983385abac1c3f2f0
Fathurrahman <10190031@bsi.ac.id>
2024-12-25 09:47:36 +0700

    fix: fetch admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,16 +1,30 @@
+import { Book, Publisher } from "@prisma/client";
 import { prisma } from "../application/database";
-import AddBook from "./add-book";
-import DeleteBook from "./delete-book";
-import UpdateBook from "./update-book";
 import TabAdminMenu from "./tab-admin-menu";
 
-const Product = async () => {
+export async function getServerSideProps() {
   const [books, publishers] = await Promise.all([
     prisma.book.findMany({
       include: { publisher: true },
     }),
     prisma.publisher.findMany(),
   ]);
+
+  return {
+    props: {
+      books,
+      publishers,
+    },
+  };
+}
+
+const Product = ({
+  books,
+  publishers,
+}: {
+  books: Book[];
+  publishers: Publisher[];
+}) => {
   return (
     <div>
       <TabAdminMenu books={books} publishers={publishers} />
